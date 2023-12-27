89954f65e20ee9cc3d7facb0a0d7097666dd862f
nishadevi46 <nishadevi4628@gmail.com>
2023-12-27 17:53:24 +0530

    sets the routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port =process.env.PORT||3000;
 
-
+app.use('/',require('./router/index'))
 
 app.listen(port,()=>{
     console.log(`connection is made on  port ${port}`);
