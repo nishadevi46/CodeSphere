79ca2ba78adee972ac82ec39dafcd64a487190a5
nishadevi46 <nishadevi4628@gmail.com>
2023-12-27 19:47:00 +0530

    set other routes and view engine

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port =process.env.PORT||3000;
 
-app.use('/',require('./router/index'))
-
+app.use('/',require('./router'))
+app.set('view engine','ejs')
+app.set('views','./views')
 app.listen(port,()=>{
     console.log(`connection is made on  port ${port}`);
-})
+}) 
--- router/index.js
+++ router/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const homeController=require("../controllers/home_controller")
 const router = express.Router();
-
+router.get('/',homeController.home)
+router.use('/users',require("./users"))
 module.exports = router;
